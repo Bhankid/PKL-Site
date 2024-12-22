1fa23ad902304796c8f6bdc39fe544f74789ae6b
Bhankid <alfredfianyo50@gmail.com>
2024-12-22 15:40:16 +0000

    Auth pages

---
+++ logout.js
@@ -0,0 +1,4 @@
+document.getElementById("logoutButton").addEventListener("click", function () {
+  // Redirect to the login page or perform any other action
+  window.location.href = "login.html"; // Change this to your login page URL
+});
